e5ffa16aa88c3d237467f69e9a2b3de705edd89e
thasnima544 <nargies2011@gmail.com>
2026-05-15 11:50:26 +0530

    Modify app to set port from environment variable
    Updated the app to use environment variable for port.

--- pune agri/app.py
+++ pune agri/app.py
@@ -121,4 +121,8 @@ def telemetry():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+
+app.run(host='0.0.0.0', port=port)
